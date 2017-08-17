a34a042c1d234b4c5086afeca2244601d10f81af
Drake Ballard <drakeballard@gmail.com>
2017-08-17 10:47:52 -0500

    Showing the moves!

--- client/js/board.js
+++ client/js/board.js
@@ -7,7 +7,25 @@ Template.board.helpers({
         side.fill(0);
 
         return side;
-    }
+    },
+    isMarked: (x, y) => {
+        if(Session.get("inGame")) {
+          let myGame = Games.findOne();
+    
+          if(myGame !== undefined && myGame.status !== "waiting") {
+            for(let i = 0; i < myGame.moves.length; i++) {
+              if(myGame.moves[i].move === x + '' + y) {
+                if(myGame.moves[i].playerID === Meteor.userId())
+                  return "<p class='mark'>X</p>";
+                else
+                  return "<p class='mark'>O</p>";
+              }
+            }
+            if(myGame.status === Meteor.userId())
+              return "<div class='selectableField' id='"+x+y+"'></div>";
+          }
+        }
+      }
 });
 
 Template.board.events({
