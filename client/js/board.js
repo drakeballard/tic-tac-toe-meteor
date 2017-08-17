6e3b161fd34911abe9f2098f396bbb4b838116f9
Drake Ballard <drakeballard@gmail.com>
2017-08-17 11:07:20 -0500

    Included the remainder and did some debugging

--- client/js/board.js
+++ client/js/board.js
@@ -1,35 +1,36 @@
 import { Meteor } from 'meteor/meteor';
-import { Template } from 'metor/templating';
+import { Template } from 'meteor/templating';
 
 Template.board.helpers({
-    sideLength: () => {
-        let side = new Array(3);
-        side.fill(0);
+  sideLength: () => {
+    let side = new Array(3);
+    side.fill(0);
 
-        return side;
-    },
-    isMarked: (x, y) => {
-        if(Session.get("inGame")) {
-          let myGame = Games.findOne();
-    
-          if(myGame !== undefined && myGame.status !== "waiting") {
-            for(let i = 0; i < myGame.moves.length; i++) {
-              if(myGame.moves[i].move === x + '' + y) {
-                if(myGame.moves[i].playerID === Meteor.userId())
-                  return "<p class='mark'>X</p>";
-                else
-                  return "<p class='mark'>O</p>";
-              }
-            }
-            if(myGame.status === Meteor.userId())
-              return "<div class='selectableField' id='"+x+y+"'></div>";
+    return side;
+  },
+
+  isMarked: (x, y) => {
+    if(Session.get("inGame")) {
+      let myGame = Games.findOne();
+
+      if(myGame !== undefined && myGame.status !== "waiting") {
+        for(let i = 0; i < myGame.moves.length; i++) {
+          if(myGame.moves[i].move === x + '' + y) {
+            if(myGame.moves[i].playerID === Meteor.userId())
+              return "<p class='mark'>X</p>";
+            else
+              return "<p class='mark'>O</p>";
           }
         }
+        if(myGame.status === Meteor.userId())
+          return "<div class='selectableField' id='"+x+y+"'></div>";
       }
+    }
+  }
 });
 
 Template.board.events({
-    "click .selectableField": (event) => {
-      Meteor.call("games.makeMove", event.target.id);
-    }
-  });
+  "click .selectableField": (event) => {
+    Meteor.call("games.makeMove", event.target.id);
+  }
+});
